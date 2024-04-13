72c92a5c47390786b46a27a30fe93be807ef3567
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-04-13 13:46:08 +0200

    fix!: fix typo in OpenMixtral8x7b model name
    BREAKING CHANGE: `Model.OpenMistral8x7b` has been renamed to `Model.OpenMixtral8x7b`.

--- src/v1/constants.rs
+++ src/v1/constants.rs
@@ -6,8 +6,8 @@ pub const API_URL_BASE: &str = "https://api.mistral.ai/v1";
 pub enum Model {
     #[serde(rename = "open-mistral-7b")]
     OpenMistral7b,
-    #[serde(rename = "open-mistral-8x7b")]
-    OpenMistral8x7b,
+    #[serde(rename = "open-mixtral-8x7b")]
+    OpenMixtral8x7b,
     #[serde(rename = "mistral-small-latest")]
     MistralSmallLatest,
     #[serde(rename = "mistral-medium-latest")]
